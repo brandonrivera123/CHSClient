c68bbf9c0e7cb20ee1afa7c6f5789f9136761006
brandonrivera123 <41973324+brandonrivera123@users.noreply.github.com>
2019-04-19 10:51:08 -0400

    Last one best one

---
+++ CHSAuction/wwwroot/js/organizationIndex.js
@@ -0,0 +1,81 @@
+﻿$(document).ready(function(){
+  $("#focusedInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#organization #organizationRow #organizationName" ).filter(function() {
+        $(this).parent().toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+
+
+    var numberOfItems = $(".organization .page").length;
+    var limitPerPage = 50;
+
+    //Limit number of items to page
+    $(".organization .page:gt(" + (limitPerPage - 1) + ")").hide();
+    var totalPages = Math.round(numberOfItems / limitPerPage);
+
+    //Append html elements to pagination class block
+    $(".pagination").append("<li class='current-page active'><a href='javascript:void(0)'>" + 1 + "</a></li>");
+    //Create appropriate number of pages
+    for (var i = 2; i <= totalPages; i++) {
+        $(".pagination").append("<li class='current-page'><a href='javascript:void(0)'>" + i + "</a></li>");
+    }
+    $(".pagination").append("<li id='next-page'><a href='javascript:void(0)' aria-label='Next'><span aria-hidden='true'>&raquo;</span></a></li>");
+
+    
+    $(".pagination li.current-page").on("click", function () {
+        if($(this).hasClass("active")) {
+            return false;
+        } 
+        else {
+            var currentPage = $(this).index();
+            $(".pagination li").removeClass("active");
+            $(this).addClass("active");
+            $(".organization .page").hide();
+
+            var grandTotal = limitPerPage * currentPage;
+
+            for (var i = grandTotal - limitPerPage; i < grandTotal; i++) {
+                $(".organization .page:eq(" + i + ")").show();
+            }
+        }
+    });
+
+    $("#next-page").on("click", function () {
+        var currentPage = $(".pagination li.active").index();
+        if (currentPage === totalPages) {
+            return false;
+        } else {
+            currentPage++;
+            $(".pagination li").removeClass("active");
+            $(".organization .page").hide();
+
+            var grandTotal = limitPerPage * currentPage;
+
+            for (var i = grandTotal - limitPerPage; i < grandTotal; i++) {
+                $(".organization .page:eq(" + i + ")").show();
+            }
+
+            $(".pagination li.current-page:eq(" + (currentPage - 1) + ")").addClass("active");
+        }
+    });
+
+    $("#previous-page").on("click", function () {
+        var currentPage = $(".pagination li.active").index();
+        if (currentPage === 1) {
+            return false;
+        } else {
+            currentPage--;
+            $(".pagination li").removeClass("active");
+            $(".organization .page").hide();
+
+            var grandTotal = limitPerPage * currentPage;
+
+            for (var i = grandTotal - limitPerPage; i < grandTotal; i++) {
+                $(".organization .page:eq(" + i + ")").show();
+            }
+
+            $(".pagination li.current-page:eq(" + (currentPage - 1) + ")").addClass("active");
+        }
+    });
+});
